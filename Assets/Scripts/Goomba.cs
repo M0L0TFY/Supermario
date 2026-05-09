b9fd69ad626e18c3a7a83a5b320ea086ad1d1e57
Mohamed Lotfy <moh.lotfy05@gmail.com>
2026-05-09 23:58:56 +0300

    feat: add enemy, player logic and game manager

---
+++ Assets/Scripts/Goomba.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Goomba : MonoBehaviour
+{
+    public Sprite flatSprite;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && collision.gameObject.TryGetComponent(out Player player))
+        {
+            if (collision.transform.DotTest(transform, Vector2.down)) {
+                Flatten();
+             } else {
+                player.Hit();
+            }
+        }
+    }
+
+    private void Flatten()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<EnemyMovement>().enabled = false;
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = flatSprite;
+        Destroy(gameObject, 0.5f);
+    }
+
+    private void Hit()
+    {
+        GetComponent<AnimatedSprite>().enabled = false;
+        //GetComponent<DeathAnimation>().enabled = true;
+        Destroy(gameObject, 3f);
+    }
+
+}
---
+++ Assets/Scripts/Koopa.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Koopa : MonoBehaviour
+{
+    public Sprite shellSprite;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && collision.gameObject.TryGetComponent(out Player player))
+        {
+            if (collision.transform.DotTest(transform, Vector2.down)) {
+                EnterShell();
+             } else {
+                player.Hit();
+            }
+        }
+    }
+
+    private void EnterShell()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<EnemyMovement>().enabled = false;
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = shellSprite;
+        Destroy(gameObject, 0.5f);
+    }
+
+    private void Hit()
+    {
+        GetComponent<AnimatedSprite>().enabled = false;
+        //GetComponent<DeathAnimation>().enabled = true;
+        Destroy(gameObject, 3f);
+    }
+
+}
